4853c881064f14de45adf2c499001c1b0e7879c3
jisooooooooooo <dlawltn0708@naver.com>
2025-05-09 23:54:10 +0900

    feat: 내 정보 수정하기 UI 구현

--- week4-task/src/pages/MyPage/MyPageInfo.tsx
+++ week4-task/src/pages/MyPage/MyPageInfo.tsx
@@ -1,3 +1,28 @@
-const MyPageInfo = () => <div>내 정보 페이지</div>;
+import {
+  container,
+  title,
+  label,
+  input,
+  button,
+  buttonActive,
+} from "../../shared/styles/formCommon.css";
+
+const MyPageInfo = () => {
+  return (
+    <div className={container}>
+      <h1 className={title}>내 정보 수정하기</h1>
+      <label htmlFor="new-nickname" className={label}>
+        새 닉네임
+      </label>
+      <input
+        id="new-nickname"
+        type="text"
+        placeholder="새 닉네임을 입력하세요"
+        className={input}
+      />
+      <button className={`${button} ${buttonActive}`}>저장</button>
+    </div>
+  );
+};
 
 export default MyPageInfo;
