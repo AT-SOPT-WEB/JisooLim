63f700a73a36bdbae1fa87564cc8815989c48007
jisooooooooooo <dlawltn0708@naver.com>
2025-05-20 13:42:42 +0900

    fix: 유틸함수 분리

--- week3-task/src/components/GithubSearch/GithubUserCard.jsx
+++ week3-task/src/components/GithubSearch/GithubUserCard.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import {
   cardStyle,
   closeButton,
@@ -9,22 +8,19 @@ import {
   followStyle,
   followBox,
 } from "../../styles/GithubSearch.style";
+import { externalLinkProps } from "../../utils/linkUtils";
 
 const GithubUserCard = ({ data, onClose, theme }) => {
-  const linkProps = {
-    target: "_blank",
-    rel: "noopener noreferrer",
-  };
   return (
     <section css={cardStyle(theme)}>
       <button type="button" css={closeButton} onClick={onClose}>
         ❌
       </button>
-      <a href={data.html_url} {...linkProps}>
+      <a href={data.html_url} {...externalLinkProps}>
         <img src={data.avatar_url} alt="프로필" css={profileImg(theme)} />
       </a>
       <h1 css={username}>
-        <a href={data.html_url} css={profileLink(theme)} {...linkProps}>
+        <a href={data.html_url} css={profileLink(theme)} {...externalLinkProps}>
           {data.name}
         </a>
       </h1>
